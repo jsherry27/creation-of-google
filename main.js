412c4baec5b9656cb8979b26da8ac5f9a4941fd5
jsherry27 <jsherry27@gmail.com>
2022-09-13 20:58:36 -0400

    Finished

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ fetch("http://localhost:3000/google").then(async (serverResponse) => {
   const data = await serverResponse.json();
   console.log(data);
 
-  //   if (data.google.length === 0) {
-  //     const containerEl = document.querySelector(".container");
-  //     const errText = document.createElement("h1");
-  //     errText.textContent = "Sir, we have a problem";
-  //     containerEl.appendChild(errText);
-  //   } else {
-
-  data.google.forEach((n) => {
+  // const dataEl = {
+  //     rogueNinja: [
+  //         1,2,3
+  //     ]
+  // };
+  // dataEl.rogueNinja.length
+  // const dataEl = [
+  //     1,2
+  // ]
+  // dataEl.forEach
+  data.forEach((n) => {
     console.log(n);
     const containerEl = document.querySelector(".container");
 
@@ -17,31 +20,29 @@ fetch("http://localhost:3000/google").then(async (serverResponse) => {
     rowEl.classList.add("row");
 
     const div1El = document.createElement("div");
+    div1El.classList.add("someone");
 
     const para1El = document.createElement("p");
-    para1El.src = n.url;
+    para1El.textContent = n.breadcrumbs;
     div1El.appendChild(para1El);
 
     const h1El = document.createElement("h1");
-    h1El.textContent = `${n.title}`;
+    const aEl = document.createElement("a");
+    aEl.textContent = n.title;
+    aEl.href = n.url;
+    aEl.target = "_blank";
+    h1El.appendChild(aEl);
     div1El.appendChild(h1El);
 
-    // const anchorEl = documenbt.createElement("a");
-    // anchorEl.getAttribute("href");
-    // anchorEl.textContent = "Ziggurat - Wikipedia";
-    // h1El.appendChild(anchorEl);
-
     const para2El = document.createElement("p");
-    para2El.textContent = `${n.description}`;
+    para2El.textContent = n.description;
     div1El.appendChild(para2El);
 
     const imgEl = document.createElement("img");
-    imgEl.classList.add("row");
-    imgEl.textContent = `${n.imageUrl}`;
-    rowEl.appendChild(imgEl);
+    imgEl.src = n.imageUrl;
 
     rowEl.appendChild(div1El);
+    rowEl.appendChild(imgEl);
     containerEl.appendChild(rowEl);
   });
-  //   }s
 });
